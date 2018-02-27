7f4d991f6a2e7d7568354880f3f1cdade314bd89
nathanwelsh8 <36056683+nathanwelsh8@users.noreply.github.com>
2018-02-27 17:29:20 +0000

    task 1 answer

---
+++ ex1,py.py
@@ -0,0 +1,16 @@
+#ex1
+
+#get user input and store in variable 'name'
+name = input("Whats your name?\n")
+
+
+# if you want to make 1st letter capital letter then do:
+name = name.title()
+
+print("Your name is "+str(name))
+
+#maybe your feeling angry?
+
+name = name.upper()
+
+#add a new print here too see what name looks like now?
